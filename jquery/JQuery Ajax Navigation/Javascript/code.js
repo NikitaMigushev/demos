430d07da6be9061b0a29e8cc6bc4fe287d5c647f
NikitaMigushev <migushev@gmail.com>
2020-02-26 09:07:53 +0300

    fix: add auto load first page ajax

--- jquery/JQuery Ajax Navigation/Javascript/code.js
+++ jquery/JQuery Ajax Navigation/Javascript/code.js
@@ -2,11 +2,13 @@ $("document").ready(function () {
 
   //Ajax page load
 
+  $("#page").load("pages.html #home");
+
   $("nav ul li a").click(function () {
 
     $("#page").load("pages.html #" + $(this).attr("href"));
     return false;
 
   });
-  
+
 });
